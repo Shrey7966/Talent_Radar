7183dfb39994ea83b5772b14f30d5c66f91007d6
Shreyas <shreyasg_96@outlook.com>
2026-04-26 23:43:40 -0500

    Real Faiss is working

--- tests/test.py
+++
@@ -1,4 +0,0 @@
-from app.resume_parser import extract_resume_text
-
-text = extract_resume_text("/Users/shreyasg/Documents/SEMESTER-2-SPRING-2026/COURSES/MIS_6V99_DATABASE/6V99_Project/data/Shreyasg_SoftwareEngineer_Internship.pdf")
-print(text[:500])
